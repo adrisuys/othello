9e663f36cf36de1997996c8e3321f53d70c36c94
s_u_y_s_a <s_u_y_s_a@guest-dyn-ip-58.wlg.esi.heb.site>
2017-12-10 12:10:03 +0100

    modif dans la classe GUIRules dans la manière d'afficher le texte

--- src/esi/atlg3/g43320/othello/view/fx/GUIRules.java
+++ src/esi/atlg3/g43320/othello/view/fx/GUIRules.java
@@ -29,22 +29,13 @@ public class GUIRules {
         title.setFont(Font.font("Verdana", 70));
         title.setTextAlignment(TextAlignment.JUSTIFY);
         Text rules = new Text();
-        rules.setText("Othello se joue à 2, sur un plateau unicolore de 64 cases (8 sur 8), avec des pions bicolores, noirs d'un côté \n"
-                + "et blancs de l'autre. Le but du jeu est d'avoir plus de pions de sa couleur que l'adversaire à la fin de  \n"
-                + "la partie, celle-ci s'achevant lorsque aucun des deux joueurs ne peut plus jouer de coup légal, généralement \n"
-                + "lorsque les 64 cases sont occupées.\n\n"
-                + "Afin de poser un pion de votre couleur sur le plateau, il suffit de cliquer sur la case désirée. Si vous \n"
-                + "pouvez jouer sur cette case, elle s'illuminera en vert clair. Si non, elle deviendra rouge. Vous pouvez \n"
-                + "également placer des murs afin de bloquer vos adversaires, pour ce faire, il suffit de faire un clic gauche \n"
-                + "sur n'importe quelle case de l'othellier.\n\n"
-                + "A votre tour, vous pouvez décider de soit jouer un pion, soit de placer un mur. Si vous ne savez pas jouer, \n"
-                + "vous aurez le choix entre passer votre tour ou placer un mur sur le plateau. \n \n"
-                + "Que le meilleur gagne!");
+        rules.setText("Othello se joue à 2, sur un plateau unicolore de 64 cases (8 sur 8), avec des pions bicolores, noirs d'un côté et blancs de l'autre. Le but du jeu est d'avoir plus de pions de sa couleur que l'adversaire à la fin de la partie, celle-ci s'achevant lorsque aucun des deux joueurs ne peut plus jouer de coup légal, généralement lorsque les 64 cases sont occupées.\n\nAfin de poser un pion de votre couleur sur le plateau, il suffit de cliquer sur la case désirée. Si vous pouvez jouer sur cette case, elle s'illuminera en vert clair. Si non, elle deviendra rouge. Vous pouvez également placer des murs afin de bloquer vos adversaires, pour ce faire, il suffit de faire un clic gauche sur n'importe quelle case de l'othellier.\n\nA votre tour, vous pouvez décider de soit jouer un pion, soit de placer un mur. Si vous ne savez pas jouer, vous aurez le choix entre passer votre tour ou placer un mur sur le plateau. \n \nQue le meilleur gagne!");
+        rules.setWrappingWidth(800);
         rules.setFont(Font.font("Verdana", 15));
         rules.setTextAlignment(TextAlignment.JUSTIFY);
         frame.getChildren().addAll(title,rules);
-        VBox.setMargin(title, new Insets(10,10,10,10));
-        VBox.setMargin(rules, new Insets(10,10,10,10));
+        VBox.setMargin(title, new Insets(10,10,10,30));
+        VBox.setMargin(rules, new Insets(10,10,10,30));
     }
 
     /**
